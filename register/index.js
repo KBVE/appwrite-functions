ba83a7dbd3cff3c27d893ae0f01fa127f4b11b27
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-09-16 00:00:22 -0400

    Testing Format

---
+++ register/index.js
@@ -0,0 +1,46 @@
+const sdk = require("node-appwrite");
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+  const errorHandler = (__errorMessage) => {
+    res.json({ data: "error", message: __errorMessage }, 400);
+  };
+
+  const validHandler = (_data) => {
+    res.json({ data: _data }, 200);
+  };
+
+  const client = new sdk.Client();
+  if (
+    !req.variables["APPWRITE_FUNCTION_ENDPOINT"] ||
+    !req.variables["APPWRITE_FUNCTION_API_KEY"] ||
+    !req.variables["APPWRITE_FUNCTION_PROJECT_ID"]
+  ) {
+    errorHandler(
+        "[Error] Environment variables are not set thus the function cannot use Appwrite SDK."
+      );
+  } else {
+    client
+      .setEndpoint(req.variables["APPWRITE_FUNCTION_ENDPOINT"])
+      .setProject(req.variables["APPWRITE_FUNCTION_PROJECT_ID"])
+      .setKey(req.variables["APPWRITE_FUNCTION_API_KEY"]);
+    //.setSelfSigned(true);
+  }
+
+  const database = new sdk.Databases(client);
+  const users = new sdk.Users(client);
+  let request = "";
+
+  if (req.payload) {
+    try {
+        request = JSON.parse(req.payload);
+    } catch (e) {
+      errorHandler(`[Error] JSON Parsing Payload -> ${e}`);
+    }
+  } else {
+    errorHandler("[Error] Payload was missing");
+  }
+
+  const { username, email, password, token } = request;
+
+};
